850eac134383f0f0c3f0173778769af086623932
Erwin <afwyousenge@gmail.com>
2021-09-16 15:54:00 +0800

    修復firebase連線問題，並新增撈資料測試程式碼OK

--- connection/firebase_admin_connect.js
+++ connection/firebase_admin_connect.js
@@ -4,7 +4,9 @@ var admin = require("firebase-admin");
 var serviceAccount = require("../expressfirebaseauthentication-firebase-adminsdk-j8lip-3cf0bf782f.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://expressfirebaseauthentication-default-rtdb.firebaseio.com"
+
 });
 
 var db = admin.database();//只要使用這個功能
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var firebaseDb = require('../connection/firebase_admin_connect')
 router.get('/', function (req, res, next) {
+    firebaseDb.ref().once('value', function (snapshop) {
+        console.log(snapshop.val())
+    })
     res.render('index', {
         title: '六角學院留言板'
     });
