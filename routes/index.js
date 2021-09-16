bd05e52554ac80d4ccbae2e83a747c6a62a0ca1e
Erwin <afwyousenge@gmail.com>
2021-09-16 16:52:37 +0800

    載入firebase模組，但無法運作

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var firebaseDb = require('../connection/firebase_admin_connect')
+var firebase = require('../connection/firebase_connect')
 router.get('/', function (req, res, next) {
+    console.log(firebase.auth())
     firebaseDb.ref().once('value', function (snapshop) {
         console.log(snapshop.val())
     })
