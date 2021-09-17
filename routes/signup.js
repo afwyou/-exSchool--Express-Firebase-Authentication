d6c9cd5876603eaf4641916caf3c0700cd137b33
afwyou <afwyousenge@gmail.com>
2021-09-18 07:38:16 +0800

    註冊邏輯設計(但網頁卡住）

--- routes/signup.js
+++ routes/signup.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('../connection/firebase_connect')
+var fireAuth = firebase.auth()
 router.get('/', function (req, res) {
-    res.render('signup', { title: '註冊'});
+    res.render('signup', { title: '註冊' });
 })
 
 router.post('/', function (req, res) {
+    var email = req.body.email
+    var password = req.body.passwd
+    fireAuth.createUserWithEmailAndPassword(email, password)
+        .then(function (user) {
+            res.redirect('/signup/success')
+        })
+        .catch(function (error) {
 
+        })
 })
-router.get('/success',function(req,res){
-    res.render('success',{
-        title:'註冊成功'
+router.get('/success', function (req, res) {
+    res.render('success', {
+        title: '註冊成功'
     });
 })
 module.exports = router;
